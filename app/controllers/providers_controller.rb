5466abac047733a50f61d2173dcb2d2815a99aaf
Marcos <marcintosh87@gmail.com>
2022-04-11 17:21:04 -0400

    created react client

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -2,6 +2,7 @@ class ProvidersController < ApplicationController
     require 'rest-client'
 
     def get_providers
+        #using rest client gem, the response from third party API is being routed to /providers_search/:npi
         npi = params[:npi]
         url = "https://npiregistry.cms.hhs.gov/api/?version=2.1&number=#{npi}"
         res = RestClient.get(url)
